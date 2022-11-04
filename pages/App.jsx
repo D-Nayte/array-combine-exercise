52befa470b7cf52f6c7b6c3fac36cc710103ab91
Andy Schunke <schunke.andy@gmail.com>
2022-11-04 17:44:35 +0100

    bugfix

--- pages/App.jsx
+++ pages/App.jsx
@@ -1,9 +1,22 @@
 import React, { useEffect, useState } from "react";
 import DisplayData from "../components/DisplayData";
-import owned from "../assets/data/ownedBooks.json";
-import searched from "../assets/data/searchedBooks.json";
 
 function App() {
+  const owned = [
+    { title: "Book1", color: "green" },
+    { title: "Book2", color: "green" },
+    { title: "Book3", color: "green" },
+    { title: "Book4", color: "green" },
+    { title: "Book7", color: "green" },
+  ];
+  const searched = [
+    { title: "Book10", color: "red" },
+    { title: "Book20", color: "red" },
+    { title: "Book90", color: "red" },
+    { title: "Book3", color: "red" },
+    { title: "Book7", color: "red" },
+  ];
+
   // write a function to compare 2 arrays, store it in "combined"
   //and display all "searched" results in red
   //if you already own the book, should been displayed in green
@@ -12,12 +25,6 @@ function App() {
   //   const combined = [
   //     { title: "Book10", color: "red" },
   //     { title: "Book20", color: "red" },
-  //     { title: "Book30", color: "red" },
-  //     { title: "Book40", color: "red" },
-  //     { title: "Book50", color: "red" },
-  //     { title: "Book60", color: "red" },
-  //     { title: "Book70", color: "red" },
-  //     { title: "Book80", color: "red" },
   //     { title: "Book90", color: "red" },
   //     { title: "Book3", color: "green" },
   //     { title: "Book7", color: "green" },
